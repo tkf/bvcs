1345a8e19bb6f72f47d81cd36186fe0927884c6b
Takafumi Arakaki <aka.tkf@gmail.com>
2012-04-22 17:35:56 +0200

    Make init/clone/commit/isclean available from CLI

--- bvcs/methods/__init__.py
+++ bvcs/methods/__init__.py
@@ -3,3 +3,7 @@ import bvcs.methods.identify
 import bvcs.methods.pull
 import bvcs.methods.dumpstate
 import bvcs.methods.checkout
+import bvcs.methods.init
+import bvcs.methods.clone
+import bvcs.methods.commit
+import bvcs.methods.isclean
