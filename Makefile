9f76437a8dd8cf49632d02a526fc503105d2b624
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-10 22:25:44 +0200

    Add bvcs/__init__.py target in Makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
+all: README bvcs/__init__.py
+
+clean:
+	rm -f README
+
 README: generate_readme.py README-header.rst
 	python generate_readme.py > README
+
+bvcs/__init__.py: README-header.rst
+	cd bvcs && cog.py -r __init__.py
--- bvcs/__init__.py
+++ bvcs/__init__.py
@@ -1,9 +1,20 @@
-# [[[cog import cog; cog.outl('"""\n%s\n"""' % file('../README.rst').read())]]]
+# [[[cog import cog
+#    cog.outl('"""\n%s\n"""' % file('../README-header.rst').read())
+# ]]]
 """
 Batched VCS commands
 ====================
 
-Run VCS commands (git/hg/bzr) asynchronously over a set of repositories.
+Run bunch of VCS commands (git/hg/bzr) over a set of repositories.
+
+Use cases:
+
+* You want to make sure you pushed changes to remote for all of your
+  repositories.
+* You want to log the revision of all of your repositories.
+* You are using git but want to have hg/bzr repositories as submodule.
+* You want to do (equivalent of) ``git fetch`` for all of your repositories.
+* etc...
 
 """
 # [[[end]]]
